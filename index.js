0e43fbf43f45b62bb31e2d776bb928e732876827
Eduardo <tkd13000@gmail.com>
2024-01-01 13:57:23 -0600

    Se agregó el menú desplegable y se le agregó el cambio de modo del fondo

---
+++ index.js
@@ -0,0 +1,16 @@
+//Botones para cambiar el color de fondo
+let botonBlanco = document.getElementById("btnlight");
+let botonNegro = document.getElementById("btndark");
+let titulo = document.getElementById("titulo");
+
+botonBlanco.addEventListener("click", function()
+{
+    document.body.style.backgroundColor = "#D2E3C8";  
+    titulo.style.color = "#191919";
+});
+
+botonNegro.addEventListener("click", function()
+{
+    document.body.style.backgroundColor = "#191919";
+    titulo.style.color = "#D2E3C8";
+});
