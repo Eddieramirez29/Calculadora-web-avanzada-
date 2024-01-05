c45f64e81c40ece69ae116eaf6f5c94a85f3ed2a
Eduardo <tkd13000@gmail.com>
2024-01-04 20:15:27 -0600

    Se agrega el botón de Sign In

--- index.js
+++ index.js
@@ -2,17 +2,20 @@
 let botonBlanco = document.getElementById("btnlight");
 let botonNegro = document.getElementById("btndark");
 let titulo = document.getElementById("titulo");
+let barraDeNavegación = document.getElementById("barraDeNavegación");
 
 botonBlanco.addEventListener("click", function()
 {
     document.body.style.backgroundColor = "#D2E3C8";  
     titulo.style.color = "#191919";
+    barraDeNavegación.style.backgroundColor =  "#D2E3C8";
 });
 
 botonNegro.addEventListener("click", function()
 {
     document.body.style.backgroundColor = "#191919";
     titulo.style.color = "#D2E3C8";
+    barraDeNavegación.style.backgroundColor =  "#191919";
 });
 
 
