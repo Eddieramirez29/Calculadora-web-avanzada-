443c4535c3a054a9e3119554b1fc12a1bd2301bb
Eduardo <tkd13000@gmail.com>
2024-01-24 20:05:49 -0600

    Se elimina el archivo aritmetica y se pasa el código de este último al archivo de teclado para facilitar el manejo de las funciones de reset de valores de las variables

--- teclado.js
+++ teclado.js
@@ -1,3 +1,8 @@
+const botonBorrar = document.getElementById("botonBorrar");
+const botonResultado = document.getElementById("botonResultado");
+let entradaNumerica = document.getElementsByClassName("pantalla_inferior");
+let cadenaExpresion = "";
+//------------------
 let cadenaEntrada = "";
 // Obtener todos los elementos con las clases "celda"
 let celdas = document.querySelectorAll('.celda');
@@ -105,12 +110,12 @@ function convertIdFromIdToNumber(id)
     {
       console.log("La cadena no cumple con el patrón especificado.");
     }
-    localStorage.setItem("cadenaEntrada", cadenaEntrada);
+    // localStorage.setItem("cadenaEntrada", cadenaEntrada);
   }
   else
   {
     console.log(cadenaEntrada);
-    localStorage.setItem("cadenaEntrada", cadenaEntrada);
+    // localStorage.setItem("cadenaEntrada", cadenaEntrada);
   }
 }
 
@@ -124,44 +129,55 @@ function contieneNumeroCadena(expresion)
   return regex.test(expresion);
 }
 
-
-
-
-
-
-//El siguiente código permite que el panel sea arrastrado
-document.addEventListener("DOMContentLoaded", function()
- {
-    var panel = document.getElementById("miPanel");
-    var isDragging = false;
-    var offset = { x: 0, y: 0 };
-
-    panel.addEventListener("mousedown", function(event) 
-    {
-      isDragging = true;
-      offset.x = event.clientX - panel.getBoundingClientRect().left;
-      offset.y = event.clientY - panel.getBoundingClientRect().top;
-    });
-
-    document.addEventListener("mousemove", function(event)
-     {
-      if (isDragging) {
-        panel.style.left = event.clientX - offset.x + "px";
-        panel.style.top = event.clientY - offset.y + "px";
-      }
-    });
-
-    document.addEventListener("mouseup", function() 
-    {
-      isDragging = false;
-    });
-  });
-
-  function resetVariables()
+  //When reloding the page, the variables of the operation will be reseted
+function resetVariables()
   {
     cadenaEntrada = "";
-    localStorage.setItem("cadenaEntrada", cadenaEntrada);
+    // localStorage.setItem("cadenaEntrada", cadenaEntrada);
   }
 
+
+  //-----------------------------------------------------------
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    // Obtener todos los elementos con las clases "celda"
+  let celdas = document.querySelectorAll('.celda');
+  
+    // Agrega un event listener a cada botón
+    celdas.forEach(function (tecla)
+   {
+      tecla.addEventListener('click', function () 
+  {
+        // Llama a la función cuando se hace clic en una tecla
+        anyClic();
+      });
+    });
+  
+    // Función que maneja el clic en una tecla
+    function anyClic()
+   {
+    cadenaExpresion = cadenaEntrada;
+    entradaNumerica[0].value = cadenaExpresion;
+      // Aquí puedes realizar cualquier acción que desees al presionar una tecla
+    }
+  });
+
+//Permite hacer cálculos aritméticos
+botonResultado.addEventListener("click", function()
+{
+    //Se obtiene la cadena de texto de la entrada de datos
+    entradaNumerica[0].value = math.evaluate(cadenaExpresion);
+});
+
+//It resets only the input screen
+botonBorrar.addEventListener("click", function()
+{
+    entradaNumerica[0].value = "";
+    cadenaEntrada = "";
+});
+
+
+  //It call resetVariables when reloding page
   window.addEventListener("beforeunload", resetVariables);
 
